9a3ad77636bb88322fafffcd7268e8729221407b
juwonsmith <juwonoladele772@gmail.com>
2022-09-09 22:49:15 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+export default function about() {
+
+  return (
+    <>
+      <Head>
+      <title>Ninja List | About</title>
+      <meta name='keywords' content='ninjas'/>
+      </Head>
+      <div>
+          <h1>about</h1>
+          <p>lorem ipsum</p>
+      </div>
+    </>
+  )
+  
+}
